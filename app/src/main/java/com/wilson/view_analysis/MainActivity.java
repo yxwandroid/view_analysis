bc671aba3168d0e8662e9d480196f7182598a7d7
wilson <yxw_android@163.com>
2020-07-14 11:25:49 +0800

    update

--- app/src/main/java/com/wilson/view_analysis/MainActivity.java
+++ app/src/main/java/com/wilson/view_analysis/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,24 +16,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
-            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
-            "Apple", "Banana", "Orange", "Watermelon",
-            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        showList();
+        setContentView(R.layout.activity_main);
+
     }
 
-    private void showList() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this, android.R.layout.simple_list_item_1, data);
-        ListView listView = findViewById(R.id.demo_lv);
-        listView.setAdapter(adapter);
-    }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
